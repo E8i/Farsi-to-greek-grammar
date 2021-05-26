1401731effea3fcf0d75a93982fe61b3a5a83daa
ebigreece <ebrahim7.hosseinpour@gmail.com>
2021-05-26 20:28:43 +0300

    Lots of Changes Done

--- app/src/main/java/com/example/farsi_to_greek_grammar/MainActivity42.java
+++ app/src/main/java/com/example/farsi_to_greek_grammar/MainActivity42.java
@@ -3,6 +3,7 @@ package com.example.farsi_to_greek_grammar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,5 +34,151 @@ public class MainActivity42 extends AppCompatActivity {
                 startActivity(in);
             }
         });
+        poulao1=findViewById(R.id.poulao1);
+        final MediaPlayer mediaPlayer1 = MediaPlayer.create(this,R.raw.poulao1);
+        poulao1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer1.start();
+            }
+        });
+        poulao2=findViewById(R.id.poulao2);
+        final MediaPlayer mediaPlayer2 = MediaPlayer.create(this,R.raw.poulao2);
+        poulao2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer2.start();
+            }
+        });
+        poulao3=findViewById(R.id.poulao3);
+        final MediaPlayer mediaPlayer3 = MediaPlayer.create(this,R.raw.poulao3);
+        poulao3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer3.start();
+            }
+        });
+        poulao4=findViewById(R.id.poulao4);
+        final MediaPlayer mediaPlayer4 = MediaPlayer.create(this,R.raw.poulao4);
+        poulao4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer4.start();
+            }
+        });
+        poulao5=findViewById(R.id.poulao5);
+        final MediaPlayer mediaPlayer5 = MediaPlayer.create(this,R.raw.poulao5);
+        poulao5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer5.start();
+            }
+        });
+        poulao6=findViewById(R.id.poulao6);
+        final MediaPlayer mediaPlayer6 = MediaPlayer.create(this,R.raw.poulao6);
+        poulao6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer6.start();
+            }
+        });
+        poulao7=findViewById(R.id.poulao7);
+        final MediaPlayer mediaPlayer7 = MediaPlayer.create(this,R.raw.poulao7);
+        poulao7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer7.start();
+            }
+        });
+        poulao8=findViewById(R.id.poulao8);
+        final MediaPlayer mediaPlayer8 = MediaPlayer.create(this,R.raw.poulao8);
+        poulao8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer8.start();
+            }
+        });
+        poulao9=findViewById(R.id.poulao9);
+        final MediaPlayer mediaPlayer9 = MediaPlayer.create(this,R.raw.poulao9);
+        poulao9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer9.start();
+            }
+        });
+        poulao10=findViewById(R.id.poulao10);
+        final MediaPlayer mediaPlayer10 = MediaPlayer.create(this,R.raw.poulao10);
+        poulao10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer10.start();
+            }
+        });
+        poulao11=findViewById(R.id.poulao11);
+        final MediaPlayer mediaPlayer11 = MediaPlayer.create(this,R.raw.poulao11);
+        poulao11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer11.start();
+            }
+        });
+        poulao12=findViewById(R.id.poulao12);
+        final MediaPlayer mediaPlayer12 = MediaPlayer.create(this,R.raw.poulao12);
+        poulao12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer12.start();
+            }
+        });
+        poulao13=findViewById(R.id.poulao13);
+        final MediaPlayer mediaPlayer13 = MediaPlayer.create(this,R.raw.poulao13);
+        poulao13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer13.start();
+            }
+        });
+        poulao14=findViewById(R.id.poulao14);
+        final MediaPlayer mediaPlayer14 = MediaPlayer.create(this,R.raw.poulao14);
+        poulao14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer14.start();
+            }
+        });
+        poulao15=findViewById(R.id.poulao15);
+        final MediaPlayer mediaPlayer15 = MediaPlayer.create(this,R.raw.poulao15);
+        poulao15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer15.start();
+            }
+        });
+        poulao16=findViewById(R.id.poulao16);
+        final MediaPlayer mediaPlayer16 = MediaPlayer.create(this,R.raw.poulao16);
+        poulao16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer16.start();
+            }
+        });
+        poulao17=findViewById(R.id.poulao17);
+        final MediaPlayer mediaPlayer17 = MediaPlayer.create(this,R.raw.poulao17);
+        poulao17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer17.start();
+            }
+        });
+        poulao18=findViewById(R.id.poulao18);
+        final MediaPlayer mediaPlayer18 = MediaPlayer.create(this,R.raw.poulao18);
+        poulao18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer18.start();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/farsi_to_greek_grammar/MainActivity43.java
+++ app/src/main/java/com/example/farsi_to_greek_grammar/MainActivity43.java
@@ -3,6 +3,7 @@ package com.example.farsi_to_greek_grammar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,5 +34,151 @@ public class MainActivity43 extends AppCompatActivity {
                 startActivity(in);
             }
         });
+        fovoume1=findViewById(R.id.fovoume1);
+        final MediaPlayer mediaPlayer1 = MediaPlayer.create(this,R.raw.fovoume1);
+        fovoume1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer1.start();
+            }
+        });
+        fovoume2=findViewById(R.id.fovoume2);
+        final MediaPlayer mediaPlayer2 = MediaPlayer.create(this,R.raw.fovoume2);
+        fovoume2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer2.start();
+            }
+        });
+        fovoume3=findViewById(R.id.fovoume3);
+        final MediaPlayer mediaPlayer3 = MediaPlayer.create(this,R.raw.fovoume3);
+        fovoume3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer3.start();
+            }
+        });
+        fovoume4=findViewById(R.id.fovoume4);
+        final MediaPlayer mediaPlayer4 = MediaPlayer.create(this,R.raw.fovoume4);
+        fovoume4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer4.start();
+            }
+        });
+        fovoume5=findViewById(R.id.fovoume5);
+        final MediaPlayer mediaPlayer5 = MediaPlayer.create(this,R.raw.fovoume5);
+        fovoume5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer5.start();
+            }
+        });
+        fovoume6=findViewById(R.id.fovoume6);
+        final MediaPlayer mediaPlayer6 = MediaPlayer.create(this,R.raw.fovoume6);
+        fovoume6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer6.start();
+            }
+        });
+        fovoume7=findViewById(R.id.fovoume7);
+        final MediaPlayer mediaPlayer7 = MediaPlayer.create(this,R.raw.fovoume7);
+        fovoume7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer7.start();
+            }
+        });
+        fovoume8=findViewById(R.id.fovoume8);
+        final MediaPlayer mediaPlayer8 = MediaPlayer.create(this,R.raw.fovoume8);
+        fovoume8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer8.start();
+            }
+        });
+        fovoume9=findViewById(R.id.fovoume9);
+        final MediaPlayer mediaPlayer9 = MediaPlayer.create(this,R.raw.fovoume9);
+        fovoume9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer9.start();
+            }
+        });
+        fovoume10=findViewById(R.id.fovoume10);
+        final MediaPlayer mediaPlayer10 = MediaPlayer.create(this,R.raw.fovoume10);
+        fovoume10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer10.start();
+            }
+        });
+        fovoume11=findViewById(R.id.fovoume11);
+        final MediaPlayer mediaPlayer11 = MediaPlayer.create(this,R.raw.fovoume11);
+        fovoume11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer11.start();
+            }
+        });
+        fovoume12=findViewById(R.id.fovoume12);
+        final MediaPlayer mediaPlayer12 = MediaPlayer.create(this,R.raw.fovoume12);
+        fovoume12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer12.start();
+            }
+        });
+        fovoume13=findViewById(R.id.fovoume13);
+        final MediaPlayer mediaPlayer13 = MediaPlayer.create(this,R.raw.fovoume13);
+        fovoume13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer13.start();
+            }
+        });
+        fovoume14=findViewById(R.id.fovoume14);
+        final MediaPlayer mediaPlayer14 = MediaPlayer.create(this,R.raw.fovoume14);
+        fovoume14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer14.start();
+            }
+        });
+        fovoume15=findViewById(R.id.fovoume15);
+        final MediaPlayer mediaPlayer15 = MediaPlayer.create(this,R.raw.fovoume15);
+        fovoume15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer15.start();
+            }
+        });
+        fovoume16=findViewById(R.id.fovoume16);
+        final MediaPlayer mediaPlayer16 = MediaPlayer.create(this,R.raw.fovoume16);
+        fovoume16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer16.start();
+            }
+        });
+        fovoume17=findViewById(R.id.fovoume17);
+        final MediaPlayer mediaPlayer17 = MediaPlayer.create(this,R.raw.fovoume17);
+        fovoume17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer17.start();
+            }
+        });
+        fovoume18=findViewById(R.id.fovoume18);
+        final MediaPlayer mediaPlayer18 = MediaPlayer.create(this,R.raw.fovoume18);
+        fovoume18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer18.start();
+            }
+        });
+
+
     }
 }
