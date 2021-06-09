b8174e70dc525062b5d96d35e6a74ce07fb68bf0
ebigreece <ebrahim7.hosseinpour@gmail.com>
2021-06-09 17:01:12 +0300

    Lots of Changes Done

--- app/src/main/java/com/example/farsi_to_greek_grammar/MainActivity28.java
+++ app/src/main/java/com/example/farsi_to_greek_grammar/MainActivity28.java
@@ -2,13 +2,36 @@ package com.example.farsi_to_greek_grammar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity28 extends AppCompatActivity {
+
+    private Button week1,week2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main28);
+
+
+        week1=findViewById(R.id.week1);
+        week1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(MainActivity28.this,MainActivity5.class);
+                startActivity(in);
+            }
+        });
+        week2=findViewById(R.id.week2);
+        week2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(MainActivity28.this,MainActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
